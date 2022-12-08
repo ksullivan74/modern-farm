b6dd81b60f1e9d6dab708a6b0fc0525c9a94a237
Neil Sullivan <ac30delay@gmail.com>
2022-12-08 11:37:28 -0600

    inital commit

---
+++ src/scripts/field.js
@@ -0,0 +1,12 @@
+/*
+1.) Define a variable in this module whose initial value is an empty array. 
+    a.) This array will store all of the plants that are growing in the field. Do not export the array.
+2.) In the module, define and export a function named addPlant.
+    a.)The addPlant function must accept a seed object as input. 
+    b.)The function will add the seed to the field (How do you add something to an array? Do you remember the method for it?).
+3.) Define and export a function named usePlants that returns a copy of the array of plants.
+Be aware that when planting corn, 
+an array of objects will be provided instead of a single object like all the other type of seeds. 
+Investigate the Array.isArray method to check if corn got passed in, or one of the others.
+*/
+
